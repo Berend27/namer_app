7a565435a8c8f0263871d9fb7be56bb71e9f41c1
solarkdexter <dexterb@sol-ark.com>
2024-05-23 12:29:14 -0500

    8. added a new page

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-// Pick back up at 8. Add anew page
 void main() {
   runApp(MyApp());
 }
@@ -62,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -147,6 +146,35 @@ class GeneratorPage extends StatelessWidget {
       ),
     );
   }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center( 
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+            '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+  
 }
 
 class BigCard extends StatelessWidget {
